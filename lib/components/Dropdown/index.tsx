bc28c97b10e44bf0c8aac80c366466a0b5572dd1
hnamhocit <hnamhocit@gmail.com>
2025-04-13 02:13:07 +0700

    fix dropdown position prop

--- lib/components/Dropdown/index.tsx
+++ lib/components/Dropdown/index.tsx
@@ -1,34 +1,34 @@
-import { cva, VariantProps } from 'class-variance-authority'
-import { AnimatePresence, motion } from 'motion/react'
-import { FC, memo, ReactNode, useRef, useState } from 'react'
+import { cva, VariantProps } from "class-variance-authority";
+import { AnimatePresence, motion } from "motion/react";
+import { FC, memo, ReactNode, useRef, useState } from "react";
 
-import { cn } from '../../utils/cn'
+import { cn } from "../../utils/cn";
 
 const content = cva(
-	'absolute top-full left-0 p-2 bg-white border border-gray-200 z-10 rounded-2xl shadow max-w-80 min-w-40 min-h-40 max-h-80 overflow-y-scroll',
+	"absolute p-2 bg-white border border-gray-200 z-10 rounded-2xl shadow max-w-80 min-w-40 min-h-40 max-h-80 overflow-y-scroll",
 	{
 		variants: {
 			position: {
-				'top-left': 'bottom-full left-0',
-				'top-right': 'bottom-full right-0',
-				'bottom-left': 'top-full left-0',
-				'bottom-right': 'top-full right-0',
-				'top-center': 'bottom-full left-1/2 -translate-x-1/2',
-				'right-center': 'top-1/2 -translate-x-1/2 left-full',
-				'left-center': 'top-1/2 -translate-y-1/2 right-full',
-				'bottom-center': 'top-full -translate-x-1/2 left-1/2',
+				"top-left": "bottom-full left-0",
+				"top-right": "bottom-full right-0",
+				"bottom-left": "top-full left-0",
+				"bottom-right": "top-full right-0",
+				"top-center": "bottom-full left-1/2 -translate-x-1/2",
+				"right-center": "top-1/2 -translate-x-1/2 left-full",
+				"left-center": "top-1/2 -translate-y-1/2 right-full",
+				"bottom-center": "top-full -translate-x-1/2 left-1/2",
 			},
 		},
 		defaultVariants: {
-			position: 'bottom-right',
+			position: "bottom-right",
 		},
 	},
-)
+);
 
 interface DropdownProps extends VariantProps<typeof content> {
-	children: ReactNode
-	trigger: ReactNode
-	className?: string
+	children: ReactNode;
+	trigger: ReactNode;
+	className?: string;
 }
 
 const Dropdown: FC<DropdownProps> = ({
@@ -37,18 +37,14 @@ const Dropdown: FC<DropdownProps> = ({
 	className,
 	position,
 }) => {
-	const [isOpen, setIsOpen] = useState(false)
-	const ref = useRef<HTMLDivElement>(null)
+	const [isOpen, setIsOpen] = useState(false);
+	const ref = useRef<HTMLDivElement>(null);
 
-	const toggleIsOpen = () => setIsOpen((prev) => !prev)
+	const toggleIsOpen = () => setIsOpen((prev) => !prev);
 
 	return (
-		<div
-			className='relative w-fit'
-			ref={ref}>
-			<div
-				className='cursor-pointer'
-				onClick={toggleIsOpen}>
+		<div className="relative w-fit" ref={ref}>
+			<div className="cursor-pointer" onClick={toggleIsOpen}>
 				{trigger}
 			</div>
 
@@ -58,13 +54,14 @@ const Dropdown: FC<DropdownProps> = ({
 						animate={{ opacity: 1, translateY: 8 }}
 						exit={{ opacity: 0, translateY: -8 }}
 						className={cn(content({ position }), className)}
-						onClick={toggleIsOpen}>
+						onClick={toggleIsOpen}
+					>
 						{children}
 					</motion.div>
 				)}
 			</AnimatePresence>
 		</div>
-	)
-}
+	);
+};
 
-export default memo(Dropdown)
+export default memo(Dropdown);
